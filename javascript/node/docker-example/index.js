9689f596b23bcb1d48d805eb9a0c9aec263141ac
Andrew Fei <andrewjfei@gmail.com>
2024-09-27 14:54:11 +1200

    updated docker example with docker comose

--- javascript/node/docker-example/index.js
+++ javascript/node/docker-example/index.js
@@ -1,10 +1,30 @@
 import express from "express";
+import pg from "pg";
+import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
+const { Client } = pg;
+const client = new Client({
+    connectionString: process.env.POSTGRESQL_URL
+});
+
 app.use("/api/hello-world", (_req, res) => {
-    res.send(JSON.stringify({ data: ["hello", "world"] }));
+    res.send(JSON.stringify({ data: "Hello, World!" }));
+});
+
+app.use("/api/now", async (_req, res) => {
+    // connect to postgresql database
+    await client.connect();
+
+    const result = await client.query("SELECT NOW()");
+
+    // disconnect from postgresql database
+    await client.end();
+
+    const now = result.rows[0].now;
+    res.send(JSON.stringify({ data: now }));
 });
 
 app.listen(port, () => {
